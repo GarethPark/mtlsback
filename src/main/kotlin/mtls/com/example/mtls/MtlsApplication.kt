a05a72f20230c79d457406513d281126b77ddf20
Gareth Park <garethpark@yahoo.com>
2025-01-11 12:18:51 +0000

    second commit

---
+++ src/main/kotlin/mtls/com/example/mtls/MtlsApplication.kt
@@ -0,0 +1,11 @@
+package mtls.com.example.mtls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MtlsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MtlsApplication>(*args)
+}
---
+++ src/test/kotlin/mtls/com/example/mtls/MtlsApplicationTests.kt
@@ -0,0 +1,13 @@
+package mtls.com.example.mtls
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MtlsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
